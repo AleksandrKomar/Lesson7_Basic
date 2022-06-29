8ca7b6acc194807f25c390543c06ef53470983b8
AK <nightmare8707@gmail.com>
2022-06-29 17:22:21 +0300

    correct

--- src/hometasks/task5/Main.java
+++ src/hometasks/task5/Main.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import hometasks.task5.Calculator;
-
-
 public class Main {
     public static void main(String[] args) throws IOException {
 
